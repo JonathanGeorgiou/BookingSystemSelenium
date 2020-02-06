08bc5ece8e803b4b8328b84fdeebdc433ba7bb12
Jonathan <j.georgiou94@gmail.com>
2020-02-06 10:53:43 +0000

    intiail commit

---
+++ src/test/java/selenium/SeleniumSetup.java
@@ -0,0 +1,25 @@
+package selenium;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class SeleniumSetup {
+
+    public static final String URL = "http://localhost/BookingSystem/index.html";
+
+
+    private WebDriver driver;
+
+    @FindBy(id ="book")
+    private WebElement bookButton;
+
+    public void navToSelectService() {
+        bookButton.click();
+    }
+}
---
+++ src/test/java/selenium/SeleniumTest.java
@@ -0,0 +1,51 @@
+package selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class SeleniumTest {
+
+    private SeleniumSetup setup;
+
+    private WebDriver driver;
+
+    @Before
+    public void before(){
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
+        this.driver = new ChromeDriver(options);
+        this.driver.manage().window().maximize();
+        this.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    public void seleniumTest() throws InterruptedException {
+        this.driver.get("http://localhost");
+
+        WebDriverWait wait = new WebDriverWait(driver,30);
+
+        setup.navToSelectService();
+        String URL = driver.getCurrentUrl();
+        assertEquals(URL, "http://localhost/BookingSystem/select-service.html" );
+
+}
+
+}
